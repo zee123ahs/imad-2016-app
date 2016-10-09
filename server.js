e4e56dcc1371f69d0661810ba8482488fbc5adef
zee123ahs <zeeshanahsan_sh@srmuniv.edu.in>
2016-10-09 16:10:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-    res.send('Article one requested and wiil be served here');
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req, res){
